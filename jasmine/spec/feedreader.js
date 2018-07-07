bbc89687b1f0f2a656a3a6cd814f6d7681c96b0d
Brianne <brianne.hennel@gmail.com>
2018-07-07 15:10:58 -0400

    'names are defined' test complete

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -36,13 +36,19 @@ $(function() {
              expect(allFeeds[i].url).toBeDefined();
              expect(allFeeds[i].url.length).not.toBe(0);
            }
-         })
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('names are defined', function() {
+           for (let i = 0; i < allFeeds.length; i++) {
+             expect(allFeeds[i].name).toBeDefined();
+             expect(allFeeds[i].name.length).not.toBe(0);
+           }
+         });
     });
 
 
