6a3ff76c116ec1931289af79a59713778abba940
Brianne <brianne.hennel@gmail.com>
2018-07-07 16:47:42 -0400

    Describe functions for additional test suites complete

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,6 +54,8 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
 
+    describe('The Menu', function() {
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -65,8 +67,10 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -74,11 +78,14 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
